2972064af5beed1002c4f4c851c234796deee1d0
jora-albatross <70007342+jora-albatross@users.noreply.github.com>
2025-10-12 21:46:48 -0400

    Update tailwind.config.cjs
    updated

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -11,10 +11,10 @@ module.exports = {
         line: "#ECEFF3",
       },
       boxShadow: {
-        soft: "0 12px 30px rgba(20,25,40,.10)"
+        soft: "0 12px 30px rgba(20,25,40,.10)",
       },
-      borderRadius: { xl: "16px" }
-    }
+      borderRadius: { xl: "16px" },
+    },
   },
-  plugins: []
-}
+  plugins: [],
+};
