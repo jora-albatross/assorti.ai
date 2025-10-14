5171f76e15d1bd960140a0c7d99ae0b2de281292
jora-albatross <70007342+jora-albatross@users.noreply.github.com>
2025-10-13 21:08:42 -0400

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import tailwind from '@astrojs/tailwind';
+import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
-  site: 'https://assorti.ai',
-  integrations: [mdx(), tailwind({ applyBaseStyles: true })],
+  integrations: [tailwind({
+    config: {
+      applyBaseStyles: true
+    }
+  })]
 });
